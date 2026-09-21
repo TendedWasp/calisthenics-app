9e3dd877bfd7fa31641caedba60021f91ee23027
TendedWasp <eaglefan4jesus@gmail.com>
2026-09-21 13:59:23 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,37 @@
+const CACHE_NAME = 'workout-store-v2';
+
+// Add your specific repository name here
+const REPO_NAME = '/calisthenics-app/'; 
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('workout-store').then((cache) => cache.addAll([
-      './index.html',
-      './manifest.json'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        REPO_NAME,
+        REPO_NAME + 'index.html',
+        REPO_NAME + 'manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  // Clear old caches when we update the app
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
